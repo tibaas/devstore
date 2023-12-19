82a931580fa77966af69c9f83886d2346b874935
tibaas <igorox2@hotmail.com>
2023-12-19 17:16:34 -0300

    Updating

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -3,7 +3,5 @@ import data from '../data.json'
 export async function GET() {
     const featuredProducts = data.products.filter((product) => product.featured)
 
-
-
     return Response.json(featuredProducts)
 }
